2386bbeb163c1f769125edb5d5bf66804fab86e9
gwen <adelerufino@gmail.com>
2020-02-09 14:36:15 -0400

    cambios 7 Servicios Basicos, rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// Rutas
 
-import { AppRoutingModule } from './app-routing.module';
+// Rutas
+import { APP_ROUTES } from './app.routes';
+
+// Modulos
+import { PagesModule } from './pages/pages.module';
+
+// temporal
+import { FormsModule } from '@angular/forms';
+
+// Servicios
+import { ServiceModule } from './services/service.module';
+
+
+
+
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { APP_ROUTES } from './app.routes';
 import { RegisterComponent } from './login/register.component';
-import {PagesModule} from './pages/pages.module';
+
+
 
 @NgModule({
   declarations: [
@@ -17,9 +31,10 @@ import {PagesModule} from './pages/pages.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     APP_ROUTES,
     PagesModule,
+    FormsModule,
+    ServiceModule
   ],
   providers: [],
   bootstrap: [AppComponent]
